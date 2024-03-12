d59663e4a34244d410c70001125015f9d2ceb116
Hondo <neilaleshire@outlook.com>
2024-03-12 13:45:46 -0400

    initial comit

---
+++ ShireOfAle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShireOfAle.Startup))]
+namespace ShireOfAle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
